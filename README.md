3c911183c108e902cf968938bee8eea5383302f3
Lucas Mila <lucas.porta@fcamara.com.br>
2019-08-14 17:42:23 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# NodeJs-Reddit-scrape
-A simple scrape from r/popular that will make a .json file for the first 10 posts
+# NodeJs Reddit Scrapper
+
+This project is nothing more than a simple scrapper for the r/popular page of reddit which will take the top10 posts at the moment and generate a json with the results containg the title, Original poster, link to the post and the subreddit.
+
+## How to use
+
+Simple use `npm i` and it will install all dependencies, and the run `node index.js`
+
+This build was made and working on 14/August/2019 (the css classes could been modified at the date you are testing)
+
+The recommended node version is on the .nvmrc(v12.7.0)
---
+++ index.js
@@ -0,0 +1,46 @@
+const puppeteer = require('puppeteer');
+const $ = require('cheerio');
+const fs = require('fs');
+const url = 'https://www.reddit.com/r/popular/';
+const posts = [];
+
+const getPage = async () => {
+  try {
+    const newPage = await puppeteer.launch();
+    const blankPage = await newPage.newPage();
+    const page = await blankPage.goto(url).then(() => blankPage.content());
+    getCards(page);
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+const getCards = async page => {
+  try {
+    $('div.scrollerItem', page).each(function() {
+      posts.push({
+        title: $('h3', this).text(),
+        subreddit: $('div._2mHuuvyV9doV3zwbZPtIPG > a._3ryJoIoycVkA88fy40qNJc', this).text(),
+        originalPoster: $('div._2mHuuvyV9doV3zwbZPtIPG > a._2tbHP6ZydRpjI44J3syuqC', this).text(),
+        linkPost:
+          'https://www.reddit.com' + $('div._3-miAEojrCvx_4FQ8x3P-s > a._1UoeAeSRhOKSNdY_h3iS1O', this).attr('href')
+      });
+    });
+    generateJSON();
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+const generateJSON = () => {
+  try {
+    fs.writeFile('./reddit.json', JSON.stringify(posts), err => {
+      if (err) throw err;
+      process.exit();
+    });
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+getPage();
